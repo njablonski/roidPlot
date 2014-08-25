fa22f3f8cf1422772e0991d3d3d74425413de941
Nick Jablonski <njablonski@palantir.com>
2014-08-25 15:41:26 -0700

    Initial commit

---
+++ roidPlot/subOutput.js
@@ -0,0 +1,12 @@
+subOutput = [
+	{"Designation": "4179", "Name": "Toutatis", "Diameter": 5.4, "MOID": 0.0060684},
+	{"Designation": "3200", "Name": "Phaethon", "Diameter": 5.1, "MOID": 0.0197883},
+	{"Designation": "3122", "Name": "Florence", "Diameter": 4.9, "MOID": 0.0453336},
+	{"Designation": "4015", "Name": "Wilson-Harrington", "Diameter": 4.0, "MOID": 0.0473449},
+	{"Designation": "1981", "Name": "Midas", "Diameter": 3.4, "MOID": 0.00408962},
+	{"Designation": "1620", "Name": "Geographos", "Diameter": 2.56, "MOID": 0.0309336},
+	{"Designation": "161989", "Name": "Cacus", "Diameter": 1.9, "MOID": 0.0161093},
+	{"Designation": "2201", "Name": "Oljato", "Diameter": 1.8, "MOID": 0.00303307},
+	{"Designation": "1862", "Name": "Apollo", "Diameter": 1.5, "MOID": 0.0257256},
+	{"Designation": "3671", "Name": "Dionysus", "Diameter": 1.5, "MOID": 0.0204595}
+	];
